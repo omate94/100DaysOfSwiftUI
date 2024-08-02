894bbf93f5ecfa3cd436c4d9b0ddca93d9cf1ec5
Oláh Máté <mate.olah@prezi.com>
2024-08-03 00:50:52 +0200

    Project 4 [BetterRest]

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Oláh Máté on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
