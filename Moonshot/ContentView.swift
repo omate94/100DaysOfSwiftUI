e837ac7b224a9157ba3c22b74d2e19aa804c284e
Oláh Máté <mate.olah@prezi.com>
2024-08-09 00:17:34 +0200

    Project 8 [Moonshot]

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Oláh Máté on 08/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                     Text(mission.displayName)
+                                         .font(.headline)
+                                         .foregroundStyle(.white)
+                                     Text(mission.formattedLaunchDate)
+                                         .font(.caption)
+                                         .foregroundStyle(.white.opacity(0.5))
+                                 }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Oláh Máté on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
