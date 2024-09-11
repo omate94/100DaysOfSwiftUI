49487e6fc602037ea5a63e55ce439906784ed6e5
Oláh Máté <mate.olah@prezi.com>
2024-09-11 22:51:23 +0200

    Project 18 [LayoutAndGeometry]

---
+++ LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,256 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Oláh Máté on 11/09/2024.
+//
+
+import SwiftUI
+
+struct AlignmentAndAlignmentGuides: View {
+    var body: some View {
+        Text("Live long and prosper")
+            .frame(width: 300, height: 300)  
+        
+        Text("Live long and prosper")
+            .frame(width: 300, height: 300, alignment: .topLeading)
+            .offset(x: 10, y: 10)
+        
+        HStack(alignment: .lastTextBaseline) {
+            Text("Live")
+                .font(.caption)
+            Text("long")
+            Text("and")
+                .font(.title)
+            Text("prosper")
+                .font(.largeTitle)
+        }
+        
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in d[.trailing] }
+            Text("This is a longer line of text")
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
+        
+//        VStack(alignment: .leading) {
+//            ForEach(0..<10) { position in
+//                Text("Number \(position)")
+//                    .alignmentGuide(.leading) { _ in Double(position) * -10 }
+//            }
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.blue)
+    }
+}
+
+struct CustomAlignmentGuide: View {
+    var body: some View {
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image(.horror)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct AbsolutePositioning: View {
+    var body: some View {
+        Text("Hello, world!")
+            .background(.red)
+            .position(x: 100, y: 100)
+        
+        Text("Hello, world!")
+            .offset(x: 50, y: 50)
+            .background(.red)
+        
+        Spacer()
+        Spacer()
+    }
+}
+
+struct GeometryReaderView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            Image(.horror)
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.8)
+        }
+        
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+            GeometryReader { proxy in
+                Image(.horror)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
+        }
+    }
+}
+
+
+struct FramesAndCoordinatesInsideGeometryReader: View {
+    var body: some View {
+        VStack {
+            GeometryReader { proxy in
+                Text("Hello, World!")
+                    .frame(width: proxy.size.width * 0.9, height: 40)
+                    .background(.red)
+            }
+            .background(.green)
+            
+            Text("More text")
+                .background(.blue)
+        }
+    }
+}
+
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct CoordinateSpaceView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
+    }
+}
+
+struct ScrollViewEffects: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewEffects2: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { proxy in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewEffects3: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                        }
+
+                }
+                .scrollTargetLayout()
+            }
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+//        AlignmentAndAlignmentGuides()
+//        CustomAlignmentGuide()
+//        AbsolutePositioning()
+//        GeometryReaderView()
+//        FramesAndCoordinatesInsideGeometryReader()
+//        CoordinateSpaceView()
+//        ScrollViewEffects()
+//        ScrollViewEffects2()
+        ScrollViewEffects3()
+    }
+}
+
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Oláh Máté on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
