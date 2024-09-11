0456f07e40977d9f56e4b89c279aadfbc5b6e7f0
Oláh Máté <mate.olah@prezi.com>
2024-09-11 23:31:06 +0200

    Project 18 + Challanges

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -227,6 +227,29 @@ struct ScrollViewEffects3: View {
     }
 }
 
+struct ChallengeView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hue: min(proxy.frame(in: .global).minY / fullView.frame(in: .global).maxY, 1.0), saturation: 1, brightness: 1))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(max(proxy.frame(in: .global).origin.y / fullView.frame(in: .global).maxY * 2, 0.5))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
 //        AlignmentAndAlignmentGuides()
@@ -237,7 +260,8 @@ struct ContentView: View {
 //        CoordinateSpaceView()
 //        ScrollViewEffects()
 //        ScrollViewEffects2()
-        ScrollViewEffects3()
+//        ScrollViewEffects3()
+        ChallengeView()
     }
 }
 
